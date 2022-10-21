b8e2f9b999fe31ebcab9d191b007a2586cc649a4
+ <+>
2022-10-21 17:55:04 +0300

    Assignment Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,32 @@
 class Person
 
+    # setter method\
+    attr_writer(name)
+    def name=(name)
+      @name = name
+    end
+  
+    # getter method
+    attr_reader(name)
+    def name
+      @name
+    end
+  
+  end
+  kanye = Person.new
+kanye.name=("Kanye")
+
+class Person
+    def name=(person_name)
+        @name=person_name
+    end
+    def name
+        @name
+    end
+    def job=(person_job)
+        @job = person_job
+    end
+    def job
+        @job
+    end
 end
